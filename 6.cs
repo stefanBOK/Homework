5e03eec6729910fb4e79c7dae1d551193aec6db9
stefanBOK <32751286+stefanBOK@users.noreply.github.com>
2018-02-15 20:09:14 +0200

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6ta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello";
+            string str2 = "World";
+            object obj = str1 + " " + str2;
+        }
+    }
+}
---
+++ 9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9ta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "The \"use\" of quotations causes difficulties.";
+            string str2 = "The " + "\u0022" + "use" + "\u0022" + " of quotations causes difficulties";
+        }
+    }
+}
