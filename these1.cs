66fba9cdb3d0012acc3e3809bc8b0985cb4b5549
stefanBOK <32751286+stefanBOK@users.noreply.github.com>
2018-02-22 20:51:31 +0200

    Add files via upload

---
+++ these1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1mhmhmthese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] words = text.Split(' ')
+                int k = words.Length - 1;
+            for (int i=k;i>=0;i--)
+            {
+                Console.WriteLine(words[i] + " ");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ these2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3mgmhibitches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a =
+            {
+                {5,24,7,5 },
+                { 3,1,35,6},
+                {1,6,234,67 },
+                {123,31,13,87 }
+            };
+            int n = (int)Math.Sqrt(a.Length);
+            for (int i =0; i<n; i++)
+            {
+                for (int j=i;j<n;j++)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+        }
+            Console.ReadLine();
+
+    }
+}
